fba50acd8efa6f5c273233edd11a15e38f5b8513
SG_Muwa <motherlode.muwa@gmail.com>
2019-05-21 09:25:02 +0300

    Первая версия

---
+++ CheckOnlineVkfaces/CheckOnlineVkfaces/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckOnlineVkfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = "";
+            using (var client = new WebClient())
+            {
+                data = client.DownloadString("https://vkfaces.com/vk/user/id302186940");
+            }
+            int begin = data.IndexOf("Последнее посещение");
+            data = data.Remove(0, begin);
+            Console.WriteLine(data);
+            Console.ReadLine();
+        }
+    }
+}
